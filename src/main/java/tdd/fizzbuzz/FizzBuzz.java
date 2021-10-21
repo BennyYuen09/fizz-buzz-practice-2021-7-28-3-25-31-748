2e8db413a57836bc1627865f06eb0c6863cb31ce
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-21 13:53:06 +0800

    [FizzBuzzWhizz][Benny] refact: change test case with BeforeEach

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,17 @@ package tdd.fizzbuzz;
 public class FizzBuzz{
     public String countOff(int order) {
         StringBuilder output = new StringBuilder();
+
         if (order % 3 == 0){
             output.append("Fizz");
         }
-        if(order % 5 == 0){
+        if (order % 5 == 0){
             output.append("Buzz");
         }
         if (order % 7 == 0){
             output.append("Whizz");
         }
+
         if (output.length() == 0){
             return String.valueOf(order);
         }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,15 +1,24 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setup() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     void should_return_order_number_when_count_off_given_normal_order_number_case() {
         //given
         int order = 1;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String expected = "1";
 
         //when
@@ -23,7 +32,6 @@ public class FizzBuzzTest {
     void should_return_fizz_when_count_off_given_multiple_number_of_3() {
         //given
         int order = 3;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String expected = "Fizz";
 
         //when
@@ -37,7 +45,6 @@ public class FizzBuzzTest {
     void should_return_buzz_when_count_off_given_multiple_number_of_5() {
         //given
         int order = 5;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String expected = "Buzz";
 
         //when
@@ -51,7 +58,6 @@ public class FizzBuzzTest {
     void should_return_whizz_when_count_off_given_multiple_number_of_7() {
         //given
         int order = 7;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String expected = "Whizz";
 
         //when
@@ -65,7 +71,6 @@ public class FizzBuzzTest {
     void should_return_multiple_case_string_when_count_off_given_multiple_number_of_3_5_7(){
         //given
         int order = 105;
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String expected = "FizzBuzzWhizz";
 
         //when
