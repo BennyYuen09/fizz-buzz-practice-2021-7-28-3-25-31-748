7c51ec76f2fa59a6e37e42a99d0e6becb0f310d1
BennyYuen09 <Benny.Yuen@agileexlab.net>
2021-10-21 11:44:39 +0800

    [FizzBuzzWhizz][Benny] test: add test case for noral case

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_order_number_when_count_off_given_normal_order_number_case() {
+        //given
+        int order = 1;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+
+        //when
+        String actual = fizzBuzz.countOff(order);
+
+        //then
+        assertEquals(expected, actual);
+    }
 }
